ddb4345df18f0561bca8404fd4168a381e042eb1
lzenczuk <lucjan.zenczuk@gmail.com>
2018-08-01 13:03:35 +0100

    Rename test classes and add match files

---
+++ match.cpp
@@ -0,0 +1,4 @@
+//
+// Created by lzenczuk on 01/08/18.
+//
+
---
+++ match.hpp
@@ -0,0 +1,8 @@
+//
+// Created by lzenczuk on 01/08/18.
+//
+
+#ifndef SOCCER_MATCH_H
+#define SOCCER_MATCH_H
+
+#endif //SOCCER_MATCH_H
